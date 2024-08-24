2da0f32cdca4b6bd9933172828c9e8d8eaf3b6c4
Tomas Harkema <tomas@harkema.io>
2024-08-24 23:17:48 +0200

    fixes

--- module/default.nix
+++ module/default.nix
@@ -6,7 +6,36 @@
   ...
 } @ attrs:
 with lib; let
-  defaultSystem = (import ./system.nix) attrs;
+  inputs = self.inputs;
+  defaultSystem = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+
+    specialArgs = {
+      inherit inputs;
+    };
+    modules = [
+      "${inputs.nixpkgs}/nixos/modules/installer/netboot/netboot-minimal.nix"
+      # ./installer.nix
+
+      (
+        {
+          lib,
+          pkgs,
+          config,
+          ...
+        }: {
+          config = {
+            boot = {
+              supportedFilesystems.zfs = lib.mkForce false;
+            };
+            system.stateVersion = config.system.nixos.release;
+            netboot.squashfsCompression = "zstd -Xcompression-level 22";
+            networking.wireless.enable = true;
+          };
+        }
+      )
+    ];
+  };
 in {
   options.boot.recovery = {
     enable = mkEnableOption "enable recovery";
